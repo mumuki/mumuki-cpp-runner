bd7c547305c6449e71400e866aa45c329e322f65
Federico Scarpa <fedescarpa@gmail.com>
2017-07-17 21:26:29 -0300

    added near captures

--- lib/feedback_hook.rb
+++ lib/feedback_hook.rb
@@ -7,44 +7,49 @@ class CppFeedbackHook < Mumukit::Hook
   end
 
   class CppExplainer < Mumukit::Explainer
+
+    def near_regex()
+      '[\t \n]* *(.*)\n[ \t]+\^'
+    end
+
     def explain_has_no_member_named(_, result)
-      (/error: '(.*)' has no member named '(.*)'/.match result).try do |it|
-        {type: it[1], target: it[2]}
+      (/error: '(.*)' has no member named '(.*)'#{near_regex}/.match result).try do |it|
+        {type: it[1], target: it[2], near: it[3]}
       end
     end
 
     def explain_was_not_declared_in_this_scope(_, result)
-      (/error: '(.*)' was not declared in this scope/.match result).try do |it|
-        {target: it[1]}
+      (/error: '(.*)' was not declared in this scope#{near_regex}/.match result).try do |it|
+        {target: it[1], near: it[2]}
       end
     end
 
     def explain_does_not_a_type(_, result)
-      (/error: '(.*)' does not name a type/.match result).try do |it|
-        {type: it[1]}
+      (/error: '(.*)' does not name a type#{near_regex}/.match result).try do |it|
+        {type: it[1], near: it[2]}
       end
     end
 
     def explain_has_incomplete_type_and_cannot_be_defined(_, result)
-      (/error: aggregate '(.*)' has incomplete type and cannot be defined/.match result).try do |it|
-        {type: it[1]}
+      (/error: aggregate '(.*)' has incomplete type and cannot be defined#{near_regex}/.match result).try do |it|
+        {type: it[1], near: it[2]}
       end
     end
 
     def explain_invalid_suffix(_, result)
-      (/error: invalid suffix "(.*)" on integer constant/.match result).try do |it|
-        {suffix: it[1]}
+      (/error: invalid suffix "(.*)" on integer constant#{near_regex}/.match result).try do |it|
+        {suffix: it[1], near: it[2]}
       end
     end
 
     def explain_expected_comma_before_token(_, result)
-      (/error: expected ',' or '\.\.\.' before '\.' token\n\s(.*)/.match result).try do |it|
+      (/error: expected ',' or '.{3}' before '\.' token#{near_regex}/.match result).try do |it|
         {near: it[1]}
       end
     end
 
     def explain_expected_initializer_before_token(_, result)
-      (/error: expected initializer before '.' token\n\s(.*)/.match result).try do |it|
+      (/error: expected initializer before '.' token#{near_regex}/.match result).try do |it|
         {near: it[1]}
       end
     end
--- spec/feedback_runner_spec.rb
+++ spec/feedback_runner_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'spec_helper'
 
-def req(content, test)
+def req(content, test='')
   struct content: content, test: test
 end
 
@@ -13,71 +13,61 @@ describe CppFeedbackHook do
   let(:feedback) {CppFeedbackHook.new.run!(request, OpenStruct.new(test_results: test_results))}
 
   context 'has_no_member_named' do
-    let(:test) {%q{
-      class MumukiTest : public CppUnit::TestFixture {
-        CPPUNIT_TEST_SUITE( MumukiTest );
-          CPPUNIT_TEST( testFoo );
-        CPPUNIT_TEST_SUITE_END();
+    let(:request) {req('class Foo {};', %q{
+      void testFoo(){
+        Foo* foo = new Foo();
+        foo->foo();
+        delete foo;
+      }
+    })}
 
-        void testFoo()
-        {
-          Foo* foo = new Foo();
-          CPPUNIT_ASSERT( foo->foo() == 4 );
-          delete foo;
-        }
-      };
-      }}
-    let(:request) {req('class Foo {};', test)}
-
-    it {expect(feedback).to eq('* Parece que no existe el campo `foo` en el tipo `class Foo`. Fijate que el nombre esté bien escrito.')}
+    it {expect(feedback).to eq('* Parece que no existe el campo `foo` en el tipo `class Foo`. Revisá en esta parte `foo->foo();` que el campo exista o el nombre esté bien escrito.')}
   end
 
   context 'was_not_declared_in_this_scope' do
-    let(:test) {%q{
-      class MumukiTest : public CppUnit::TestFixture {
-        CPPUNIT_TEST_SUITE( MumukiTest );
-          CPPUNIT_TEST( testFoo );
-        CPPUNIT_TEST_SUITE_END();
+    let(:request) {req('void bar() {foo == 4;}')}
 
-        void testFoo() {
-          CPPUNIT_ASSERT( foo == 4 );
-        }
-      };
-      }}
-    let(:request) {req('', test)}
-
-    it {expect(feedback).to eq('* El identificador `foo` no existe. Fijate si está bien escrito o si estás usando la variable correcta.')}
+    it {expect(feedback).to eq('* El identificador `foo` no existe. Revisá en esta parte `void bar() {foo == 4;}` si está bien escrito o si estás usando la variable correcta.')}
   end
 
   context 'does_not_a_type' do
-    let(:request) {req('doble a = 0.0;', '')}
+    let(:request) {req('doble a = 0.0;')}
 
-    it {expect(feedback).to eq('* No hay un tipo de datos con el nombre `doble`. Fijate si está bien escrito.')}
+    it {expect(feedback).to eq('* No hay un tipo de datos con el nombre `doble`. Revisá en esta parte `doble a = 0.0;` si está bien escrito.')}
   end
 
-
   context 'has_incomplete_type_and_cannot_be_defined' do
-    let(:request) {req('struct Registro; Registro variable;', '')}
+    let(:request) {req('struct Registro; Registro variable;')}
 
-    it {expect(feedback).to eq('* Parece que no definiste el tipo `Registro variable`. Fijate que el nombre esté bien escrito.')}
+    it {expect(feedback).to eq('* Parece que no definiste el tipo `Registro variable`. Revisá en esta parte `struct Registro; Registro variable;` que el nombre esté bien escrito.')}
   end
 
   context 'invalid_suffix' do
-    let(:request) {req('void 2foo() {}', '')}
+    let(:request) {req('void 2foo() {}')}
 
-    it {expect(feedback).to eq('* Parece que intentaste definir un identificador `foo` pero lo empezaste con un número. Recordá que los nombres de los identificadores deben comenzar con una **letra**, **$** o **_** (guión bajo).')}
+    it {expect(feedback).to eq('* Parece que intentaste definir un identificador `foo` no válido. Revisá en esta parte `void 2foo() {}` si lo empezaste con un número. Recordá que los nombres de los identificadores deben comenzar con una **letra**, **$** o **_** (guión bajo).')}
   end
 
   context 'expected_comma_before_token' do
-    let(:request) {req('void foo(int a.b) {}', '')}
+    let(:request) {req('void foo(int a.b) {}')}
 
-    it {expect(feedback).to eq('* El nombre de uno de los parámetros está mal. Fijate por acá `void foo(int a.b) {}` que no pueden tener puntos en el nombre.')}
+    it {expect(feedback).to eq('* El nombre de uno de los parámetros está mal. Revisá en esta parte `void foo(int a.b) {}` que no tenga puntos en el nombre.')}
   end
 
   context 'expected_initializer_before_token' do
-    let(:request) {req('int a.b;', '')}
+    let(:request) {req('int a.b;')}
 
-    it {expect(feedback).to eq('* El nombre de una de las variables está mal. Fijate por acá `int a.b;` que no pueden tener puntos en el nombre.')}
+    it {expect(feedback).to eq('* El nombre de una de las variables está mal. Revisá en esta parte `int a.b;` que no tenga puntos en el nombre.')}
+  end
+
+  context 'when same error occurs more than once  times in the same line' do
+    let(:request) {req(%q{
+      struct Foo;
+      void Bar(SDL_Surface *screen, struct Foo);
+      void Baz(SDL_Surface *screen, struct Foo);
+    })}
+
+    it {expect(feedback).to eq('* El identificador `screen` no existe. Revisá en esta parte `void Bar(SDL_Surface *screen, struct Foo);` si está bien escrito o si estás usando la variable correcta.')}
   end
 
 end
